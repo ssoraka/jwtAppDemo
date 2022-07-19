c616080f1736d5a6e179fbaae12ca18b314c7ef0
root <ssoraka@student.21-school.ru>
2022-07-19 23:50:28 +0300

    Authentication

---
+++ src/main/java/net/example/jwtappdemo/rest/AuthenticationRestControllerV1.java
@@ -0,0 +1,35 @@
+package net.example.jwtappdemo.rest;
+
+import net.example.jwtappdemo.security.jwt.JwtTokenProvider;
+import net.example.jwtappdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/auth")
+public class AuthenticationRestControllerV1 {
+
+    private AuthenticationManager authenticationManager;
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    private UserService userService;
+
+    @Autowired
+    public AuthenticationRestControllerV1(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserService userService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+    }
+
+    public ResponseEntity login(@RequestBody Object object) {
+        //1.03.31
+        return null;
+    }
+
+
+}
